b465608f6d97891ac6d7b34a726746b1d921cf7e
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-06 16:18:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
-#terraform {
-    #required_providers {
-       # aws = {
-            #source  = "hashicorp/aws"
-           # version = "~> 3.0"
-      #  }
-    #}
-#}
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
 
-#provider "aws" {
-    #region = "eu-west-2"
-#}
+provider "aws" {
+    access_key=var.access_key
+    secret_key=var.secret_key
+    region = "eu-west-2"
+}
 
 resource "aws_instance" "nginx" {
     ami = "ami-0194c3e07668a7e36"
