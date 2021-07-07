ae04bd2c28113a947d07e75cfe64c6ce153fd6a0
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-07 18:59:39 +0100

    Update main.tf

--- Terraform/EKS/main.tf
+++ Terraform/EKS/main.tf
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "project_node_group" {
   node_role_arn = var.node_iam_role_arn
   subnet_ids = var.subnet_ids
   scaling_config {
-    desired_size = 3
+    desired_size = 1
     max_size = 4
     min_size = 1
   }
