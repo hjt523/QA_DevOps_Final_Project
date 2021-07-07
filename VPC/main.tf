78a6e5026684a8e635287736b57ead95b2ace2a8
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-07 11:08:41 +0100

    Delete VPC directory

--- VPC/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "vpc" {
-    cidr_block = var.vpc_cidr_block
-    enable_dns_hostnames = true
-    tags = {
-        Name = "Final_PROJ"
-    }
-}
--- VPC/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc_cidr_block" {
-    description = "CIDR block for VPC"
-}
