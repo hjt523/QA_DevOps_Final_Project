1f17a82cae755e8bb1ec4e0d75945e6db81e4508
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-07 11:04:37 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,29 +1,41 @@
 provider "aws" {
-    access_key = $access_key
-    secret_key = $secret_key
+    # access_key = var.access_key
+    # secret_key = var.secret_key
     region = "eu-west-2"
+    shared_credentials_file = "~/.aws/credentials"
 }
 
-resource "aws_instance" "nginx" {
-    ami = "ami-0194c3e07668a7e36"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "NginX"
-    }
+module "vpc" {
+    source = "./VPC"
+    vpc_cidr_block = "172.160.0.0/16"
 }
 
-resource "aws_instance" "frontend" {
-    ami = "ami-0194c3e07668a7e36"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "Frontend"
-    }
+module "subnet" {
+    source = "./SUBNET"
+    vpc_id = module.vpc.vpc_id
+    vpc_cidr_block = module.vpc.vpc_cidr_block
 }
 
-resource "aws_instance" "backend" {
-    ami = "ami-0194c3e07668a7e36"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "Backend"
-    }
+module "iam" {
+    source = "./IAM"
 }
+
+module "eks" {
+    source = "./EKS"
+    subnet_ids = module.subnet.subnet_ids
+    cluster_iam_role_arn = module.iam.cluster_iam_role
+    node_iam_role_arn = module.iam.node_iam_role
+    available_subnets = [module.subnet.subnet_1, module.subnet.subnet_2]
+}
+
+module "instances" {
+    source = "./EC2"
+}
+
+module "rds" {
+    source = "./RDS"
+    db_password=var.db_password
+}
+
+
+
--- secret.tfvars
+++ secret.tfvars
@@ -1,3 +1,3 @@
-access_key =  "$access_key"
-secret_key = "$secret_key"
-db_password = "$db_password"
+access_key =  "AKIAUTUU7XYWX7BKQB5B"#"$access_key"
+secret_key = "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"#"$secret_key"
+db_password = "rootroot"#"$db_password"
