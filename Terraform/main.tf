6692652a1f84ed23548583efe9d778a9648db07e
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-08 14:31:43 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_dynamodb_table" "terraform-locks" {
 data "terraform_remote_state" "main" {
     backend = "s3"
     config = {
-        bucket  = "terraform-state-files-230950" #"terraform-state-files-26398476"
+        bucket  = "terraform-state-files-26398476"
         key     = "./terraform.tfstate"
         region  = "eu-west-2"
     }
